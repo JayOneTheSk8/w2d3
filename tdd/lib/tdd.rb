dff3880bec6738f743df1065327cfd4dec5c9175
Justin <misterjustincox@gmail.com>
2018-10-03 18:28:52 -0400

    Finish #my_transpose

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -2,4 +2,29 @@ class Array
   def my_uniq
     self.reduce([]) { |uniques, el| uniques.include?(el) ? uniques : uniques << el }
   end
+
+  def two_sum
+    pairs = []
+    self.each_index do |start_index|
+      (start_index + 1...self.length).each do |end_index|
+        pairs << [start_index, end_index] if self[start_index] + self[end_index] == 0
+      end
+    end
+    pairs
+  end
+end
+
+def my_transpose(array)
+  transposed = []
+  row = 0
+  while row < array.length
+    transposed[row] = []
+    col = 0
+    while col < array.length
+      transposed[row] << array[col][row]
+      col += 1
+    end
+    row += 1
+  end
+  transposed
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -1,7 +1,23 @@
 require 'tdd'
 
-describe "#my_uniq" do
+describe '#my_uniq' do
   it 'removes duplicates in an array' do
     expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
+  end
+end
+
+describe '#two_sum' do
+  it 'returns all indexes where the elements sum to zero' do
+    expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+  end
+
+  it "returns the indexes in sorted order" do
+    expect([-1, 0, 2, -2, 1].two_sum).to eq([-1, 0, 2, -2, 1].two_sum.sort)
+  end
+end
+
+describe '#my_transpose' do
+  it 'transposes an array' do
+    expect(my_transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]])).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
   end
 end
